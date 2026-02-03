01dce6b15c1c8f777d12ff8c266f6a03ced502a2
gulmezeren2-byte <gulmezeren2-byte@users.noreply.github.com>
2026-02-03 15:37:54 +0300

    Fix Jest Firebase ESM error by updating transformIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
         '^.+\\.[jt]sx?$': 'babel-jest',
     },
     transformIgnorePatterns: [
-        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|firebase)',
+        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|firebase|@firebase)',
     ],
 };
